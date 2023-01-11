3ef2aa58ae3671443fe11ad59b7157e765554347
nikosajadi <na.sajadii.29@gmail.com>
2023-01-11 21:04:30 +0100

    Animation On Scroll

--- navbar.js
+++ navbar.js
@@ -1,1 +1,6 @@
-once: false, // whether animation should happen only once - while scr
+ // whether animation should happen only once - while scr
+function toggleNavbar(collapseID) {
+    document.getElementById(collapseID).classList.toggle("hidden");
+    document.getElementById(collapseID).classList.toggle("block");
+  }
+ // alert('stop')
